ca709a2d5ebaa0533923e77a8ba4f19d314872a3
bethodog <bethodog@yahoo.com.br>
2025-06-19 05:34:35 -0300

    Commit do projeto desafioControleFluxo

---
+++ src/desafioControleFluxo/contador/exception/ParametrosInvalidosException.java
@@ -0,0 +1,14 @@
+package desafioControleFluxo.contador.exception;
+
+public class ParametrosInvalidosException extends Exception {
+	
+	public ParametrosInvalidosException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ParametrosInvalidosException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
